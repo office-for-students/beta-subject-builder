3d28e987477338dcaaef281614e5e62f51c1e510
nshumoogum <nathan.shumoogum@methods.co.uk>
2019-08-27 13:50:06 +0100

    Add unit test coverage when validating header row

--- SubjectBuilderBlobTrigger/validate.py
+++ SubjectBuilderBlobTrigger/validate.py
@@ -5,21 +5,26 @@ def column_headers(header_row):
     logging.info(f"Validating header row, headers: {header_row}")
     header_list = header_row.split(",")
 
-    valid = True
-    if header_list[0] != "code":
-        logging.info(f"got in code: {header_list[0]}")
-        valid = False
+    try:
 
-    if header_list[1] != "english_label":
-        logging.info(f"got in english_label: {header_list[1]}")
-        valid = False
+        valid = True
+        if header_list[0] != "code":
+            logging.info(f"got in code: {header_list[0]}")
+            valid = False
 
-    if header_list[2] != "level":
-        logging.info(f"got in level: {header_list[2]}")
-        valid = False
+        if header_list[1] != "english_label":
+            logging.info(f"got in english_label: {header_list[1]}")
+            valid = False
 
-    if header_list[3] != "welsh_label":
-        logging.info(f"got in welsh_label: {header_list[3]}")
+        if header_list[2] != "level":
+            logging.info(f"got in level: {header_list[2]}")
+            valid = False
+
+        if header_list[3] != "welsh_label":
+            logging.info(f"got in welsh_label: {header_list[3]}")
+            valid = False
+    except IndexError:
+        logging.exception(f"index out of range\nheader_row:{header_row}")
         valid = False
 
     return valid
